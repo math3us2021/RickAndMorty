ababeb1b424b260b6dfbe9d777b15fe1b2933366
math3us2021 <matheusbf.mf@gmail.com>
2023-05-22 17:32:18 -0300

    test

--- src/pages/login.js
+++ src/pages/login.js
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         padding: 10,
         marginVertical: 10,
-        width: '80%',
+        width: '81%',
     },
     button:{
         backgroundColor: '#3498db',
